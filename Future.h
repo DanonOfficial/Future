215b97acaabf88427e34bb475c91132ad5061bda
Daniil Smolyakov <danonofficial@gmail.com>
2017-09-28 22:03:55 +0300

    Update Future.h

--- Future.h
+++ Future.h
@@ -22,16 +22,6 @@ struct Mailbox {
 };
 
 template<typename T>
-struct Mailbox<T &> {
-    std::reference_wrapper<T> a;
-    std::exception_ptr exception_ptr;
-    bool isReady = false;
-    std::mutex mute;
-    std::condition_variable condition;
-};
-
-
-template<typename T>
 class Future {
 public:
     Future(Future &&past) : data(std::move(past.data)) {};
@@ -51,62 +41,14 @@ private:
     std::shared_ptr<Mailbox<T>> data;
 };
 
-
-template<typename T>
-struct Future<T &> {
-public:
-    Future(Future &&past) : data(std::move(past.data)) {};
-
-    Future(std::shared_ptr<Mailbox<T>> data) : data(data) {};
-
-    const T &Get();
-
-    bool IsReady() const;
-
-    void Wait();
-
-    ~Future() = default;
-
-private:
-
-    std::shared_ptr<Mailbox<T>> data;
-};
-
-
 template<typename T>
 class Promise {
 public:
     Promise() : data(new Mailbox<T>), futureAlreadyGetted(false) {};
-
-    //Promise(T &smth): data(std::move(smth)),futureAlreadyGetted(false){}
-
+    
     Promise(Promise<T> &&past) : data(std::move(past.data)), futureAlreadyGetted(false) {};
 
     Future<T> GetFuture();
-
-    void Set(const T &dataToSet);
-
-    void Set(T &&dataToSet);
-
-    void SetException(const std::exception_ptr &exc);
-
-    ~Promise() = default;
-
-private:
-    bool futureAlreadyGetted;
-    std::shared_ptr<Mailbox<T>> data;
-};
-
-template<typename T>
-struct Promise<T &> {
-public:
-    Promise() : data(new Mailbox<T>), futureAlreadyGetted(false) {};
-
-    //Promise(T &smth): data(std::move(smth)),futureAlreadyGetted(false){}
-
-    Promise(Promise<T> &&past) : data(std::move(past.data)), futureAlreadyGetted(false) {};
-
-    Future<T &> GetFuture();
 
     void Set(const T &dataToSet);
 
