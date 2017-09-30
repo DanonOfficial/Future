8ca41847da31a9aa7762e07d378928e4366f3666
Daniil Smolyakov <danonofficial@gmail.com>
2017-09-30 16:19:45 +0300

    Update Future.h

--- Future.h
+++ Future.h
@@ -33,53 +33,6 @@ private:
     std::shared_ptr<ShareState<T>> data;
 };
 
-
-template<typename T>
-struct Future<T &> {
-public:
-    Future(Future &&past) : data(std::move(past.data)) {};
-
-    Future(std::shared_ptr<ShareState<T>> data) : data(data) {};
-
-    const T &Get();
-
-    bool IsReady() const;
-
-    void Wait();
-
-    ~Future() = default;
-
-private:
-
-    std::shared_ptr<ShareState<T>> data;
-};
-
-
-template<typename T>
-class Promise {
-public:
-    Promise() : data(new ShareState<T>), futureAlreadyGetted(false) {};
-
-    //Promise(T &smth): data(std::move(smth)),futureAlreadyGetted(false){}
-
-    Promise(Promise<T> &&past) : data(std::move(past.data)), futureAlreadyGetted(false) {};
-
-    Future<T> GetFuture();
-
-    void Set(const T &dataToSet);
-
-    void Set(T &&dataToSet);
-
-    void SetException(const std::exception_ptr &exc);
-
-    ~Promise() = default;
-
-private:
-    bool futureAlreadyGetted;
-    std::shared_ptr<ShareState<T>> data;
-};
-
-
 #include "FutureImplementation.hpp"
 
 
