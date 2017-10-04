69d19858aafce65d2fc717865cc723745faf6ad0
Daniil Smolyakov <danonofficial@gmail.com>
2017-10-04 22:06:41 +0300

    Add <T&> specialization

---
+++ FutureImplementation.hpp
@@ -0,0 +1,61 @@
+//
+// Created by danonofficial on 27.09.17.
+//
+
+template<typename T>
+void Future<T>::Wait() {
+    if (data == nullptr) {
+        throw error::NoData("There is no shared data at adress");
+    }
+    std::unique_lock<std::mutex> lock(data->mute);
+    data->condition.wait(lock, [this] { return data->isReady; });
+}
+
+template<typename T>
+const T &Future<T>::Get() {
+    Wait();
+    auto temp = std::move(data);//data == nullptr after it
+    if (temp->exception_ptr) {
+        std::rethrow_exception(temp->exception_ptr);
+    }
+    return std::move(temp->value);
+}
+
+template<typename T>
+bool Future<T>::IsReady() const {
+    if (data == nullptr) {
+        return false;
+    }
+    std::unique_lock<std::mutex> lock(data->mute);
+    return data->isReady;
+}
+
+
+
+template<typename T>
+void Future<T&>::Wait() {
+    if (data == nullptr) {
+        throw error::NoData("There is no shared data at adress");
+    }
+    std::unique_lock<std::mutex> lock(data->mute);
+    data->condition.wait(lock, [this] { return data->isReady; });
+}
+
+template<typename T>
+const T &Future<T&>::Get() {
+    Wait();
+    auto temp = std::move(data);//data == nullptr after it
+    if (temp->exception_ptr) {
+        std::rethrow_exception(temp->exception_ptr);
+    }
+    return std::move(temp->value);
+}
+
+template<typename T>
+bool Future<T&>::IsReady() const {
+    if (data == nullptr) {
+        return false;
+    }
+    std::unique_lock<std::mutex> lock(data->mute);
+    return data->isReady;
+}
