02c1750c2f5643b2f1dbcc4406fe79c99b25a453
Daniil Smolyakov <danonofficial@gmail.com>
2017-10-05 21:15:54 +0300

    Updated <T&> specialization

---
+++ FutureImplementation.hpp
@@ -0,0 +1,68 @@
+//
+// Created by danonofficial on 27.09.17.
+//
+
+template<typename T>
+Future<T&>& Future<T&>::operator=(const Future<T&> &b){
+    *this = b;
+}
+;
+
+template<typename T>
+void Future<T>::Wait() {
+    if (data == nullptr) {
+        throw error::NoData("There is no shared data at adress");
+    }
+    std::unique_lock<std::mutex> lock(data->mute);
+    data->condition.wait(lock, [this] { return data->isReady; });
+}
+
+template<typename T>
+const T &Future<T>::Get() {
+    Wait();
+    auto temp = std::move(data);//data == nullptr after it
+    if (temp->exception_ptr) {
+        std::rethrow_exception(temp->exception_ptr);
+    }
+    return std::move(temp->value);
+}
+
+template<typename T>
+bool Future<T>::IsReady() const {
+    if (data == nullptr) {
+        return false;
+    }
+    std::unique_lock<std::mutex> lock(data->mute);
+    return data->isReady;
+}
+
+
+
+template<typename T>
+void Future<T&>::Wait() {
+    if (data == nullptr) {
+        throw error::NoData("There is no shared data at adress");
+    }
+    std::unique_lock<std::mutex> lock(data->mute);
+    data->condition.wait(lock, [this] { return data->isReady; });
+}
+
+template<typename T>
+T& Future<T&>::Get() {
+    Wait();
+    auto temp = std::move(data);//data == nullptr after it
+    if (temp->exception_ptr) {
+        std::rethrow_exception(temp->exception_ptr);
+    }
+    T res = *temp->value;
+    return res;
+}
+
+template<typename T>
+bool Future<T&>::IsReady() const {
+    if (data == nullptr) {
+        return false;
+    }
+    std::unique_lock<std::mutex> lock(data->mute);
+    return data->isReady;
+}
---
+++ SharedState.h
@@ -0,0 +1,26 @@
+//
+// Created by danonofficial on 30.09.17.
+//
+
+#ifndef FUTURE_SHAREDSTATE_HPP
+#define FUTURE_SHAREDSTATE_HPP
+
+template<typename T>
+struct ShareState {
+    T value;
+    std::exception_ptr exception_ptr;
+    bool isReady = false;
+    std::mutex mute;
+    std::condition_variable condition;
+};
+
+template<typename T>
+struct ShareState <T&>{
+    T *value;
+    std::exception_ptr exception_ptr;
+    bool isReady = false;
+    std::mutex mute;
+    std::condition_variable condition;
+};
+
+#endif //FUTURE_SHAREDSTATE_HPP
